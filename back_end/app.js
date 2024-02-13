5f355660478ef02cf376d34ced84d23d17d29db1
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-13 18:04:24 -0500

    Use urlencoded middleware

--- back_end/app.js
+++ back_end/app.js
@@ -1,10 +1,13 @@
-import express, { json } from 'express';
+import express, { json, urlencoded } from 'express';
 import cors from 'cors';
 import routes from './src/routes/routes.js';
 const app = express();
 
 // Enable CORS
 app.use(cors());
+
+// Parse incoming requests with URL-encoded payloads
+app.use(urlencoded({ extended: true }));
 
 // Parse request body
 app.use(json());
