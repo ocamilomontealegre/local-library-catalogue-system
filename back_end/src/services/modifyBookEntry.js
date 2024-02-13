1b8482f6ebc3566bcc81685c1466811b75cb2dcb
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-13 18:10:12 -0500

    Add service logic

---
+++ back_end/src/services/modifyBookEntry.js
@@ -0,0 +1,18 @@
+import { findBookIndex } from '../utils/functions.js';
+
+// Modify book entry properties
+export const modifyBookEntry = (bookId, updatedBookData) => {
+  const nonEmptyProperties = Object.entries(updatedBookData)
+    .reduce((acc, [key, value]) => {
+      if(value !== '') acc[key] = value;
+      return acc;
+    }, {});
+
+  try {
+    const bookIndex = findBookIndex(bookId);
+    Object.assign(database[bookIndex], nonEmptyProperties);
+    return { message: database[bookIndex] };
+  } catch (err) {
+    return { message: err.message };
+  };
+};
