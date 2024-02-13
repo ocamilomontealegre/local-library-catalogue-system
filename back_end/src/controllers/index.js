988e9e77c0e8e0da92918e9b81678ec42923aad8
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-13 18:05:15 -0500

    export getBooks method

--- back_end/src/controllers/index.js
+++ back_end/src/controllers/index.js
@@ -1,4 +1,5 @@
 export { checkApiHealth } from './checkApiHealth.js';
+export { getBooks } from './getBooks.js';
 export { addBook } from './addBook.js';
 export { updateBook } from './updateBook.js';
 export { searchBook } from './searchBook.js';
