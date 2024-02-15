8e4d092ab77b913f2dd71c95cc4cb20cc27f0009
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-15 12:52:06 -0500

    Update database module

--- back_end/src/utils/database.js
+++ back_end/src/utils/database.js
@@ -24,5 +24,77 @@ export const database = [
     releaseYear: 1981,
     pages: 122,
     picture: 'https://i.postimg.cc/mgp9g68f/03-Cr-nica.jpg'
+  },
+  {
+    id: randomUUID(),
+    title: 'One Hundred Years of Solitude',
+    author: 'Gabriel García Márquez',
+    releaseYear: 1967,
+    pages: 417,
+    picture: 'https://i.postimg.cc/C12VywF2/000-no-cover.jpg'
+  },
+  {
+    id: randomUUID(),
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    releaseYear: 1960,
+    pages: 281,
+    picture: 'https://i.postimg.cc/C12VywF2/000-no-cover.jpg'
+  },
+  {
+    id: randomUUID(),
+    title: '1984',
+    author: 'George Orwell',
+    releaseYear: 1949,
+    pages: 328,
+    picture: 'https://i.postimg.cc/C12VywF2/000-no-cover.jpg'
+  },
+  {
+    id: randomUUID(),
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    releaseYear: 1925,
+    pages: 180,
+    picture: 'https://i.postimg.cc/C12VywF2/000-no-cover.jpg'
+  },
+  {
+    id: randomUUID(),
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+    releaseYear: 1813,
+    pages: 279,
+    picture: 'https://i.postimg.cc/C12VywF2/000-no-cover.jpg'
+  },
+  {
+    id: randomUUID(),
+    title: 'The Catcher in the Rye',
+    author: 'J.D. Salinger',
+    releaseYear: 1951,
+    pages: 277,
+    picture: 'https://i.postimg.cc/C12VywF2/000-no-cover.jpg'
+  },
+  {
+    id: randomUUID(),
+    title: 'The Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+    releaseYear: 1954,
+    pages: 1178,
+    picture: 'https://i.postimg.cc/C12VywF2/000-no-cover.jpg'
+  },
+  {
+    id: randomUUID(),
+    title: 'Crime and Punishment',
+    author: 'Fyodor Dostoevsky',
+    releaseYear: 1866,
+    pages: 671,
+    picture: 'https://i.postimg.cc/C12VywF2/000-no-cover.jpg'
+  },
+  {
+    id: randomUUID(),
+    title: 'Harry Potter and the Philosopher\'s Stone',
+    author: 'J.K. Rowling',
+    releaseYear: 1997,
+    pages: 223,
+    picture: 'https://i.postimg.cc/C12VywF2/000-no-cover.jpg'
   }
 ];
